2c72bceb30c9ee97943c5727ed860abdb094a839
GregoireAntoine <60960340+GregoireAntoine@users.noreply.github.com>
2022-08-20 11:21:30 +0200

    Add files via upload

---
+++ www/data.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE testDB;
+CREATE TABLE Produit (Id int,Nom varchar(255),Prix int);
+INSERT INTO  Produit  VALUES (1,'Jouet 1',29);
+INSERT INTO  Produit  VALUES (2,'Jouet 2',59);
+INSERT INTO  Produit  VALUES (3,'Jouet 3',14);
